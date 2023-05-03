f88b6f54152cb10238ceeb55d2468dab91582ac5
Shanky43 <shankarshanky43@gmail.com;git config --global user.name Shanky43git config --global user.email shankarshanky43@gmail.com>
2023-05-03 16:30:31 +0530

    day1_fw22_0850

---
+++ fontend/src/App.js
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar";
+import Allroutes from "./routes/Allroutes";
+
+
+function App() {
+  return (
+    <div>
+      {/* <Allroutes /> */}
+      <Sidebar />
+
+    </div>
+  );
+}
+
+export default App;
