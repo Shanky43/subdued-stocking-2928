67c8da796769d0fbf187a6642e6f3b3ab3e22b9f
Shankar S <112791993+Shanky43@users.noreply.github.com>
2023-05-06 16:40:35 +0530

    Add files via upload

---
+++ frontend/src/Pages/Homeo_Pathy.js
@@ -0,0 +1,270 @@
+export const SpotlightAds = [
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/htixcxvd91ofrcfaoyzq/bjain-nux-vomica-dilution-200-ch.jpg",
+        name: "Bjain Nux Vomica Dilution 200 CH",
+        description: "bottle 12ml Dilution",
+        rating: 4.6,
+        mrp: 65,
+        offer: "9% off",
+        price: 59,
+        users_rating: "109 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/42ea5450-ef22-4a3d-9e49-2c110a710708/bjain-omeo-allergy-tablet.jpeg",
+        name: "Bjain Omeo Allergy Tablet",
+        description: "bottle of 25gm tablets",
+        rating: 4.1,
+        mrp: 155,
+        offer: "14% off",
+        price: 133,
+        users_rating: "45 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/a0ea7747911d4c5da4a116699b76628c/bjain-omeo-diabeteez-drop.jpg",
+        name: "Bjain Omeo Diabeteez Drop",
+        description: "bottle of 30ml Drop",
+        rating: 3.8,
+        mrp: 150,
+        offer: "14% off",
+        price: 120,
+        users_rating: "14 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/i6vpnss1mvrivh2gmsfu/bjain-baryta-carbonica-dilution-30-ch.jpg",
+        name: "Bjain Baryta Carbonica Dilution 30 CH",
+        description: "bottle 30ml Dilution",
+        rating: 4.8,
+        mrp: 95,
+        offer: "12% off",
+        price: 83,
+        users_rating: "30 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/10ab19e2f18a4e519a704fbaf25e7208/bjain-omeo-kalmegh-drop.jpg",
+        name: "Bjain Omeo Kalmegh Drop",
+        description: "bottle 30ml Drop",
+        rating: 4.5,
+        mrp: 95,
+        offer: "13% off",
+        price: 82,
+        users_rating: "28 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/prgnisrvwtxwq6spyqp3/bjain-lycopodium-clavatum-dilution-200-ch.jpg",
+        name: "Bjain Lycopodium Clavatum Dilution 200 CH",
+        description: "bottle 12ml Dilution",
+        rating: 4.8,
+        mrp: 65,
+        offer: "14% off",
+        price: 56,
+        users_rating: "49 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/b16131c0ed114c3291ce34518b917d9b/bjain-tinospora-cordifolia-mother-tincture-q.jpg",
+        name: "Bjain Tinospora Cordifolia Mother Tincture Q",
+        description: "bottle 100ml Mother Tincture",
+        rating: 4.2,
+        mrp: 195,
+        offer: "9% off",
+        price: 176,
+        users_rating: "29 ratings"
+    },
+    {
+        image: "https://onemg.gumlet.io/images/q_auto,w_150,h_150,f_auto,c_fit/0c37a1afd60a4062bfdd67a9c70e79be/bjain-andrographis-paniculata-mother-tincture-q.jpg",
+        name: "Bjain Andrographis Paniculata Mother Tincture Q",
+        description: "bottle 100ml Mother Tincture",
+        rating: 4.5,
+        mrp: 195,
+        offer: "9% off",
+        price: 168,
+        users_rating: "29 ratings"
+    }
+
+]
+export const popularbrands = [
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/a24d39b3-be87-42fe-a9c7-81e0137dc8e8.png", name: "ADEL Homeopathy" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/j1mwpwwh6hwegd4ecbah.png", name: "Dr Willmar Schwabe India " },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/mfswtc9scqikisqs5x9u.png", name: "SBL Homeopathy" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/emvykrq48e0lojdqjnmy.png", name: "Bakson's" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/jjn8u1l625epfbw7kbhi.png", name: "BJAIN Homeopathy" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/alty1c84eogjq2hfz3q0.png", name: "Dr. Batra's Products" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/lvysji0eofj38ghn1j5v.png", name: "Dr Reckeweg" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/470dd15e-564d-4335-a633-6146d0239858.png", name: "Medisynth" },
+    { image: "https://onemg.gumlet.io/h_150,w_150,a_ignore,q_auto,f_auto,c_fit/gpr2zunhramcprkush5j.png", name: "Wheezal" }
+]
+export const ShopByConcern = [
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/c2ymdg1tavs7mcnomcy0.png", name: "Warts" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/rpgkuklyd2youea8fdyr.png", name: "Hair Care" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/bfhia3yw4eemlxdslkks.png", name: "Sexual Wellness" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/nq5fthflkzfoahebdlv4.png", name: "Fungal Infection" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/uachuw80oilbpqnqmefp.png", name: "Diabetes" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/fdgokqurlq4c4jck8xpe.png", name: "Hypothyroidism" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/mvllnlsj1ttxuuarsfei.png", name: "Obesity" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/blzqsofglmrd8xlakfhu.png", name: "Piles and Fissures" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/wfchhzujp0duz7ir5e9w.png", name: "Allergic Rhinitis" }
+]
+export const ShopByHomeopathy = [
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/p9e2jc60m3r5nbysyfm0.png", name: "Berberis Aquifolium" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/wqsh5tn0qwxoqsvsxm16.png", name: "Berberis Vulgaris" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/sijdkmnjf3ih8k4z04co.png", name: "Nux Vomica" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/jkdrio4gxrdmelci2yre.png", name: "Arinca Montana" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/liabuc0dqnft3frvk2o0.png", name: "Syxygium Jambolanum" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/wd7et3tgcqks3ipxswhw.png", name: "Silicea" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/dosvy0feaoni0znz1tdz.png", name: "Thyroidinum" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/fcgg3l4cei3adzzgzdre.png", name: "Cineraria Maritima" },
+    { image: "https://onemg.gumlet.io/a_ignore,f_auto,w_150,q_auto,h_150,c_fit/vnjqwoktfbdmk5af1q7t.png", name: "Phytolacca Berry" }
+]
+export const brands = [
+    { brand: "SBL Pvt ltd.", numbers: "1250" },
+    { brand: "Dr. Roshanlal Aggarwal", numbers: "1500" },
+    { brand: "Tonicard", numbers: "50" },
+    { brand: "Dr. Reckeweg", numbers: "215" },
+    { brand: "ADEL", numbers: "1452" },
+    { brand: "Boiren", numbers: "214" }
+]
+export const discount = [
+    { brand: "High to Low", numbers: "1350" },
+    { brand: "Low to High", numbers: "1500" },
+    { brand: "less than 10%", numbers: "350" },
+    { brand: "10 and above", numbers: "715" },
+    { brand: "20 and above", numbers: "1547" },
+]
+export const age = [
+    { age: "All", numbers: "135550" },
+    { age: "Child", numbers: "1900" },
+    { age: "Elderly", numbers: "350" },
+    { age: "Adult", numbers: "640" }
+]
+export const gender = [
+    { gender: "Unisex", numbers: "15550" },
+    { gender: "Female", numbers: "1200" },
+    { gender: "male", numbers: "1350" }
+]
+export const cities = [
+    { city: "Bengaluru" },
+    { city: "Mysuru" },
+    { city: "Hubballi-Dharwad" },
+    { city: "Mangaluru" },
+    { city: "Belagavi" },
+    { city: "Shivamogga" },
+    { city: "Ballari" },
+    { city: "Davanagere" },
+    { city: "Tumakuru" },
+    { city: "Kalaburagi" },
+    { city: "Raichur" },
+    { city: "Hassan" },
+    { city: "Udupi" },
+    { city: "Chikkamagaluru" },
+    { city: "Bidar" },
+    { city: "Gadag-Betageri" },
+    { city: "Hospet" },
+    { city: "Haveri" },
+    { city: "Chitradurga" },
+    { city: "Kolar" },
+    { city: "Bagalkote" },
+    { city: "Ranebennuru" },
+    { city: "Sirsi" },
+    { city: "Karwar" },
+    { city: "Yadgir" },
+    { city: "Bhadravati" },
+    { city: "Kundapura" },
+    { city: "Chikkaballapur" },
+    { city: "Gangavathi" },
+    { city: "Gokak" },
+    { city: "Koppal" },
+    { city: "Jewargi" },
+    { city: "Kollegal" },
+    { city: "Athani" },
+    { city: "Sira" },
+    { city: "Nanjangud" },
+    { city: "Mulbagal" },
+    { city: "Mudhol" },
+    { city: "Sidlaghatta" },
+    { city: "Shahabad" },
+    { city: "Sindhnur" },
+    { city: "Sakleshpur" },
+    { city: "Tiptur" },
+    { city: "Tirumakudal Narsipur" },
+    { city: "Ron" },
+    { city: "Mudigere" },
+    { city: "Muddebihal" },
+    { city: "Madikeri" },
+    { city: "Mandya" },
+    { city: "Malavalli" },
+    { city: "Magadi" },
+    { city: "Lakshmeshwar" },
+    { city: "Lingsugur" },
+    { city: "Kushalnagar" },
+    { city: "Kodungallur" },
+    { city: "Maddur" },
+    { city: "Mudabidri" },
+    { city: "Manvi" },
+    { city: "Navalgund" },
+    { city: "Narasimharajapura" },
+    { city: "Nanjangud" },
+    { city: "Nargund" },
+    { city: "Nelamangala" },
+    { city: "Pandavapura" },
+    { city: "Piriyapatna" },
+    { city: "Puttur" },
+    { city: "Rabkavi Banhatti" },
+    { city: "Ramanagaram" },
+    { city: "Raybag" },
+    { city: "Robertson Pet" },
+    { city: "Sankeshwara" },
+    { city: "Saundatti-Yellamma" },
+    { city: "Savanur" },
+    { city: "Sedam" },
+    { city: "Shahpur" },
+    { city: "Shiggaon" },
+    { city: "Shrirangapattana" },
+    { city: "Siddapur" },
+    { city: "Srinivaspur" },
+    { city: "Terdal" },
+    { city: "Talikota" },
+    { city: "Tarikere" },
+    { city: "Tekkalakote" },
+    { city: "Terdal" },
+    { city: "Thiruvananthapur" }
+]
+export const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 800 },
+        items: 3
+    },
+    ipad: {
+        breakpoint: { max: 800, min: 500 },
+        items: 3
+    },
+    mobile: {
+        breakpoint: { max: 500, min: 0 },
+        items: 1
+    }
+};
+export const responsive1 = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 800 },
+        items: 4
+    },
+    ipad: {
+        breakpoint: { max: 800, min: 500 },
+        items: 3
+    },
+    mobile: {
+        breakpoint: { max: 500, min: 0 },
+        items: 2
+    }
+};
+export const filterCategories = [
+    { item: "BRAND", value: "brand" },
+    { item: "AGE", value: "age" },
+    { item: "GENDER", value: "gender" }
+]
