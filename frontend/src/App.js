b657b3967fcae9d10860d51f19746aa4854b924c
Disha Gupta <115460391+DishaGup@users.noreply.github.com>
2023-05-07 23:36:13 +0530

    sorting,filtering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import Allroutes from "./routes/Allroutes";
 function App() {
   return (
     <div id='app-body' >
-      <Navbar/>
-      <Allroutes />
+      {/* <Navbar/>
+      <Allroutes /> */}
     </div>
 
   );
