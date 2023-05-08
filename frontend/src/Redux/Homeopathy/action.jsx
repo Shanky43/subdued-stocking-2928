0e118d451f3be79a2cc950f09c8e4a69001a31c6
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-05-09 02:49:36 +0530

    update data

--- frontend/src/Redux/Homeopathy/action.jsx
+++ frontend/src/Redux/Homeopathy/action.jsx
@@ -1,17 +1,27 @@
-
 import axios from "axios";
-import { GET_PRODUCT_FAILURE, GET_PRODUCT_REQUEST, GET_PRODUCT_SUCCESS } from "./actionType";
+import {
+  GET_PRODUCT_FAILURE,
+  GET_PRODUCT_REQUEST,
+  GET_PRODUCT_SUCCESS,
+} from "./actionType";
+
+import {
+  POST_PRODUCT_FAILURE,
+  POST_PRODUCT_REQUEST,
+  POST_PRODUCT_SUCCESS,
+} from "./actionType";
 // import queryString from 'query-string';
 
-export const homeopathyProducts = (param) => (dispatch) => {
+export const homeopathyProducts = (params) => (dispatch) => {
   dispatch({ type: GET_PRODUCT_REQUEST });
 
-  console.log('API Params:', param);
- 
-  axios.get(`http://localhost:8080`,  param )
+  console.log("API Params:", params);
+
+  axios
+    .get(`http://localhost:8080`, params)
     .then((res) => {
       let filteredData = res.data;
-      console.log(res.data.products)
+      console.log(res.data.products);
       // Apply filters
       if (params.has("brand")) {
         filteredData = filteredData.filter((product) =>
@@ -49,33 +59,26 @@ export const homeopathyProducts = (param) => (dispatch) => {
       //   filteredData = filteredData.sort((a, b) => a.price - b.price);
       // }
 
-      dispatch({ type: GET_PRODUCT_SUCCESS,payload:res.data });
+      dispatch({ type: GET_PRODUCT_SUCCESS, payload: res.data });
     })
     .catch((err) => {
       dispatch({ type: GET_PRODUCT_FAILURE });
     });
+};
 
-}
-
-
-import { POST_PRODUCT_FAILURE, POST_PRODUCT_REQUEST, POST_PRODUCT_SUCCESS } from "./actionType";
 // import queryString from 'query-string';
 
 export const CartProducts = () => (dispatch) => {
   dispatch({ type: POST_PRODUCT_REQUEST });
 
-  console.log('API Params:', );
- 
-  axios.get(`http://localhost:8080`,   )
-    .then((res) => {
+  console.log("API Params:");
 
-      dispatch({ type: POST_PRODUCT_SUCCESS,payload:res.data });
+  axios
+    .get(`http://localhost:8080`)
+    .then((res) => {
+      dispatch({ type: POST_PRODUCT_SUCCESS, payload: res.data });
     })
     .catch((err) => {
       dispatch({ type: POST_PRODUCT_FAILURE });
     });
-
-}
-
-
-
+};
